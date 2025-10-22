f845d30379ab2ea0edb53584648fb6de4becfbed
Juan Pablo Caniupan Yori <61192655+juancaniupan@users.noreply.github.com>
2025-10-21 23:51:14 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webContratoQR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
